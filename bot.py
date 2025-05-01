455359038a52571befdf4b751aac3298f317c809
Sanjay Jogi <95102194+sanjayjogi@users.noreply.github.com>
2025-05-01 14:34:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,3 +78,6 @@ class Bot(Client):
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
+        
+import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1002317423458
